891ed61547909e5446494545db2b89acdabd2962
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-01-22 17:45:39 -0300

    Creando variables del formulario

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function() {
+    "use strict";
+    document.addEventListener('DOMContentLoaded', function() {
+
+        //Campos datos usuarios
+        const nombre = document.getElementById('nombre');
+        const apellido = document.getElementById('apellido');
+        const email = document.getElementById('email');
+
+
+        //Campos Pases
+
+        const paseDia = document.getElementById('pase_dia');
+        const paseCompleto = document.getElementById('pase_completo');
+        const paseDosDias = document.getElementById('pase_dosdias');
+
+
+
+        //Botones y Divs
+
+        const calcular = document.getElementById('calcular');
+        const errorDiv = document.getElementById('error');
+        const botonRegistro = document.getElementById('btn-registro');
+        const resultado = document.getElementById('lista-productos');
+
+
+    });
+
+})()
