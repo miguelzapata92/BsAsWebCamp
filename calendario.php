eb14d1f3ca7abcae5a7b5628b157ab7808046ade
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-06-04 19:33:57 -0300

    Creando la conexion a base de datos y calendario.php

---
+++ calendario.php
@@ -0,0 +1,11 @@
+<?php include_once 'includes/templates/header.php';?>
+    <!--Menu-->
+
+
+
+    <section class="seccion contenedor">
+        <h2>Calendario de Eventos</h2>
+
+    </section>
+
+    <?php include_once 'includes/templates/footer.php';?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -70,7 +70,7 @@
 
             <nav class="navegacion-principal clearfix">
                 <a href="conferencia.php">Conferencia</a>
-                <a href="#">Calendario</a>
+                <a href="calendario.php">Calendario</a>
                 <a href="#">Invitados</a>
                 <a href="registro.php">Reservaciones</a>
             </nav>
