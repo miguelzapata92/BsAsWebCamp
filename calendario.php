cb2ffecb3d344ed24a34b4ce3521567117033e7f
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-06-04 20:15:16 -0300

    Enlazando la base de datos con php

--- calendario.php
+++ calendario.php
@@ -5,7 +5,28 @@
 
     <section class="seccion contenedor">
         <h2>Calendario de Eventos</h2>
-
+        <?php 
+            try{
+                require_once('includes/funciones/db_conexion.php');
+                $sql = "SELECT * FROM eventos";
+                $resultado = $conn->query($sql);
+            } catch (\Exception $e){
+                echo $e->getMessage();
+            }
+        
+        ?>
+        <div class="calendario">
+            <?php 
+                $eventos = $resultado->fetch_assoc(); 
+            ?>
+            <pre>
+                <?php var_dump($eventos); ?>
+            </pre>
+        </div>
+        <?php 
+            $conn->close();
+        ?>
     </section>
+
 
     <?php include_once 'includes/templates/footer.php';?>
