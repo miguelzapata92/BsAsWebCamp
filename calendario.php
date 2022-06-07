becd65ea5105582bedb8ecbdec7d8da342aa1181
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-06-07 19:00:03 -0300

    Uniendo tablas de categoria evento con inner join

--- calendario.php
+++ calendario.php
@@ -8,7 +8,13 @@
         <?php 
             try{
                 require_once('includes/funciones/db_conexion.php');
-                $sql = "SELECT * FROM eventos";
+                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+                $sql .= " FROM eventos ";
+                $sql .= " INNER JOIN categoria_evento ";
+                $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+                $sql .= " INNER JOIN invitados ";
+                $sql .= " ON eventos.id_inv = invitados.id_invitado ";
+                $sql .= " ORDER BY evento_id ";
                 $resultado = $conn->query($sql);
             } catch (\Exception $e){
                 echo $e->getMessage();
@@ -17,11 +23,12 @@
         ?>
         <div class="calendario">
             <?php 
-                $eventos = $resultado->fetch_assoc(); 
-            ?>
-            <pre>
-                <?php var_dump($eventos); ?>
-            </pre>
+               while ($eventos = $resultado->fetch_assoc() ) { ?>
+                    <pre>
+                         <?php var_dump($eventos); ?>
+                    </pre>
+               <?php } ; ?> 
+            
         </div>
         <?php 
             $conn->close();
